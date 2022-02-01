1c7f09923f75330a65b92ec257408f2d527cbd5f
Cot-Lot <tranminhhao7599@gmail.com>
2022-02-01 18:07:47 +0700

    update

--- src/com/ute/mathutil/MathUtilLive.java
+++ src/com/ute/mathutil/MathUtilLive.java
@@ -11,10 +11,10 @@ public class MathUtilLive {
      */
     public static void main(String[] args) {
         long result = MathUtil.getFactorial(5);
-        System.out.println("expected: 5! = 120; actual: " + result);
-        System.out.println("expected: 6! = 120; actual: " + MathUtil.getFactorial(6));
-        System.out.println("expected: 7! = 120; actual: " + MathUtil.getFactorial(7));
-        System.out.println("expected: 0! = 120; actual: " + MathUtil.getFactorial(0));
+        System.out.println("expected: 5! = 120;  actual: " + result);
+        System.out.println("expected: 6! = 720;  actual: " + MathUtil.getFactorial(6));
+        System.out.println("expected: 7! = 5040; actual: " + MathUtil.getFactorial(7));
+        System.out.println("expected: 0! = 1;    actual: " + MathUtil.getFactorial(0));
 
 
     }
